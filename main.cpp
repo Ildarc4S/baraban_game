a02ea97fec9a2b419fe04555608b5eb816ea2fb7
Ildarc4S <ildarc4S@gmail.com>
2024-12-23 20:48:06 +0300

    Add state and class

--- main.cpp
+++ main.cpp
@@ -1,89 +1,123 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <random>
+#include <memory>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(1000, 1000), "Tiled Texture Example");
+enum class TypeState {
+    Wait,
+    Rotation,
+    CountResult
+};
 
-    sf::Texture texture;
-    if (!texture.loadFromFile("three.png")) {
-        return -1;
+class Button {
+public:
+    Button(float x, float y, float width, float heigth) {
+        button_shape.setSize(sf::Vector2(width, heigth));
+        button_shape.setPosition(x, y);
+        button_shape.setFillColor(sf::Color(0, 255, 0));
     }
-    texture.setRepeated(true);
-    texture.setSmooth(true);
+    void draw(sf::RenderWindow& window) {
+        window.draw(button_shape);
+    }
+private:
+    sf::RectangleShape button_shape;
+};
 
-    sf::RectangleShape rectangle(sf::Vector2f(250.f, 600.f));
-    rectangle.setTexture(&texture);
-
-    sf::RectangleShape rectangle_two(sf::Vector2f(250.f, 600.f));
-    rectangle_two.setTexture(&texture);
-    rectangle_two.setPosition(rectangle.getPosition().x + rectangle.getSize().x + 10, rectangle.getPosition().y);
-
-
-    sf::RectangleShape button(sf::Vector2f(200.f, 200.f));
-    button.setFillColor(sf::Color(0, 255, 0));
-
-    button.setPosition(rectangle_two.getPosition().x + rectangle_two.getSize().x + 10, rectangle_two.getPosition().y);
-
-    sf::Clock clock;
-    float i = 0.0f;
-    float speed = 100.0f;
-    int d = -1;
-    float speed_kof = 1.0f;
-    int stop = 0;
-    while (window.isOpen()) {
-        sf::Vector2i mousePos = sf::Mouse::getPosition(window); 
-        sf::Event event;
+class State {
+public:
+    virtual void handleInput(sf::RenderWindow& window, sf::Event& event) {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
-
-            if(event.type == sf::Event::MouseButtonPressed) {
-                if(event.mouseButton.button == sf::Mouse::Left) {
-                    if(button.getGlobalBounds().contains(mousePos.x, mousePos.y)){
-                        if(d == -1) {
-                            button.setFillColor(sf::Color(255, 0, 0));
-                            speed = 100.0f;
-                            speed_kof = 1.0f;
-                            d = -d;
-                        }
-                        else {
-                            button.setFillColor(sf::Color(0, 255, 0));
-                            speed_kof = -speed_kof;
-                        }
-                    }
-                }
-            }
         }
+    };
+    virtual void draw(sf::RenderWindow& window) = 0;
+    virtual void update(float delta_time) = 0;
+    virtual ~State() {};
+};
 
-        float deltaTime = clock.restart().asSeconds();
-        window.clear();
-        rectangle.setTextureRect(sf::IntRect(0, i+2*795/3, 300, 795));
-        // if(i >= 795) {
-        //     i = 0;
-        // }
-        
-        // rectangle.setTextureRect(sf::IntRect(0, i, 300, 550));
-        // rectangle_two.setTextureRect(sf::IntRect(0, i, 300, 550));
-        // if(d == 1) {
-        //     i += speed * deltaTime;
-        //     speed += speed_kof;
-        // }
+class StateMachine {
+public:
+    StateMachine() {
+        setState(TypeState::Wait);
+    }
+    void run(sf::RenderWindow& window);
+    void setState(TypeState type_state);
+private:
+    std::unique_ptr<State> current_state;
+};
 
-        // if(speed <= 100.0f && d == 1) {
-        //     d = -d;
-        //     stop = 1;
-        // }
-        
-        // if(stop && i < 794/3) {
-        //     i += speed * deltaTime;
-        // }
+class WaitState: public State {
+    void handleInput(sf::RenderWindow& window, sf::Event& event) override {
+        State::handleInput(window, event);
+    }
+    void draw(sf::RenderWindow& window) override {
 
-        // std::cout << speed << " " << d << " " << i << std::endl;
-        window.draw(rectangle); 
-        window.draw(rectangle_two);
-        window.draw(button);
-        window.display();
     }
 
-    return 0;
+    void update(float delta_time) {
+
+    }
+};
+
+class RotateState: public State {
+    void handleInput(sf::RenderWindow& window, sf::Event& event) override {
+        State::handleInput(window, event);
+    }
+    
+    void draw(sf::RenderWindow& window) override {
+
+    }
+
+    void update(float delta_time) {
+        
+    }
+};
+
+class CountResultState: public State {
+    void handleInput(sf::RenderWindow& window, sf::Event& event) override {
+        State::handleInput(window, event); 
+    }
+
+    void draw(sf::RenderWindow& window) override {
+
+    }
+
+    void update(float delta_time) {
+        
+    }
+};
+
+
+void StateMachine::run(sf::RenderWindow& window) {
+    sf::Event event;
+    Button b(0, 0, 100, 100);
+    while(window.isOpen()) {
+        current_state->handleInput(window, event);
+        window.clear();
+        b.draw(window);
+        window.display();
+    }
+}
+
+void StateMachine::setState(TypeState type_state) {
+    switch(type_state) {
+        case TypeState::Wait:
+            current_state = std::make_unique<WaitState>();
+            break;
+        case TypeState::Rotation:
+            current_state = std::make_unique<RotateState>();
+            break;
+        case TypeState::CountResult:
+            current_state = std::make_unique<CountResultState>();
+            break;
+    }
+}
+
+int main() {
+  sf::RenderWindow window(sf::VideoMode(1000, 1000), "MBG");
+  StateMachine state_machine;
+  state_machine.run(window);
+
+  return 0;
 }
